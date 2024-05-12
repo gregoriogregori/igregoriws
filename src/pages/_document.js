110a3bb7eca41550c605f9e5033a85d9a51f855e
GiuliaPuntoni <94111753+GiuliaPuntoni@users.noreply.github.com>
2024-05-12 11:52:59 +0200

    Prova tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,16 +15,17 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
-      </Head>
-      <Script id="gtm" strategy="afterInteractive">
-        {`
+        <Script id="gtm" strategy="afterInteractive">
+          {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-5LGK36M');
       `}
-      </Script>
+        </Script>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
