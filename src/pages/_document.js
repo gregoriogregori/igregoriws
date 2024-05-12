0217e7c43bb784233640e175465c2988b3f16d1f
GiuliaPuntoni <94111753+GiuliaPuntoni@users.noreply.github.com>
2024-05-12 11:44:19 +0200

    Add tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,9 +15,28 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5LGK36M');
+      `}
+      </Script>
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: ` <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-5LGK36M"
+      height="0"
+      width="0"
+      style="display:none;visibility:hidden"
+    ></iframe>`,
+          }}
+        />
       </body>
     </Html>
   );
